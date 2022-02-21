7bc9fdb3d820a6170bdc0a9adf8fdfb1ca19b68c
Dylan E. Holland <salinson1138@gmail.com>
2022-02-20 21:15:01 -0800

    INF-1: Fixed IS_DARWIN on Linux (Meta/Common.sh) (#2)
    INF-1: Fixed IS_DARWIN on Linux (Meta/Common.sh)

--- Meta/Common.sh
+++ Meta/Common.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-export IS_DARWIN=`[[ $(uname -s) == "Darwin" ]] && exit 0 || exit 1`
-if $IS_DARWIN; then
+export IS_DARWIN=`[ "$(uname)" = "Darwin" ]`
+
+if [ $IS_DARWIN ]; then
     NUM_CORES=`sysctl -n hw.ncpu`;
 else
-    NUM_CORES=`nproc --all`;
+    NUM_CORES=`grep processor /proc/cpuinfo | wc -l`;
 fi;
