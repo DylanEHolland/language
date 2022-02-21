86cdec195674c45d1b8f7cebfebb1f63798f10af
DylanEHolland <salinson1138@gmail.com>
2022-02-20 19:56:16 -0800

    Updated .gitignore, making changed to Meta/Build.sh

--- Meta/build.sh
+++ Meta/build.sh
@@ -10,3 +10,11 @@ cd Build && \
 cmake .. && \
 cd - && \
 make -j$NUM_CORES -C Build;
+
+
+if [ ! -d Build/bin ]; then
+    mkdir Build/bin;
+    for exe in lizas lizvm; do 
+        cp -v Build/Source/Tools/$exe/$exe Build/bin;
+    done;
+fi;
