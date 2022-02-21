9ceb359124a05205976b71ca6ba4f4832b2b9e62
DylanEHolland <salinson1138@gmail.com>
2022-02-20 20:59:40 -0800

    INF-1: Last time testing github action :(

--- Meta/Common.sh
+++ Meta/Common.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export IS_DARWIN=`[[ `uname` == "Linux" ]] && exit 0 || exit 1`
+export IS_DARWIN=[[ `uname` == "Linux" ]] && exit 0 || exit 1
 if $IS_DARWIN; then
     NUM_CORES=`sysctl -n hw.ncpu`;
 else
