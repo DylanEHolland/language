638ee8a33615226f8729d5892ffb1409badc7d24
DylanEHolland <salinson1138@gmail.com>
2022-02-20 21:13:09 -0800

    INF-1: Last attempt to fix IS_DARWIN before moving on for the night

--- Meta/Common.sh
+++ Meta/Common.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-export IS_DARWIN=`[[ $(uname) == "Darwin" ]] && exit 0 || exit 1`
-if $IS_DARWIN; then
+export IS_DARWIN=`[ "$(uname)" = "Darwin" ]`
+
+if [ $IS_DARWIN ]; then
     NUM_CORES=`sysctl -n hw.ncpu`;
 else
     NUM_CORES=`grep processor /proc/cpuinfo | wc -l`;
